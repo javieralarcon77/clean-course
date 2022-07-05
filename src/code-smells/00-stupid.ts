5edd3470d74d213d0ee2212fc39de9b9b9443bdf
Javier Alarcon <ja.ge.al.al@pragma.com.co>
2022-07-05 09:53:49 -0500

    add code smells

---
+++ src/code-smells/00-stupid.ts
@@ -0,0 +1,10 @@
+/**
+ * STUPID
+ * S: Singleton
+ * T: Tigth Coupling: alto acoplamiento
+ * U: Untestability: codigo no testeable (unit test)
+ * P: Premature optimization
+ * I: Indiscriptive naming: nombres poco descriptivos
+ * D: Duplication: duplicidad de código, no aplicar el principio
+ */
+
